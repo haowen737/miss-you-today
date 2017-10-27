cf785e8e96c029fea42a7840545dea473ed2bf5b
Haowen <zt452268020@live.com>
2017-10-27 17:42:31 +0800

    update

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
-import Animate from 'rc-animate';
-
 import { CSSTransitionGroup } from 'react-transition-group';
 
 import Dashboard from './../dashboard/Dashboard';
-import page404 from './../404/404';
+import Page404 from './../404/404';
 
 import logo from './logo.svg';
 import './App.css';
@@ -22,21 +20,19 @@ const AppWelcome = () => (
   </div>
 )
 
+const AppMain = () => (
+  <Switch>
+    <Route exact path="/" component={AppWelcome}></Route>
+    <Route path="/dashboard" component={Dashboard}></Route>
+    <Route component={Page404}/>
+  </Switch>
+)
+
 class App extends Component {
   render() {
     return (
       <div>
-        <CSSTransitionGroup
-        transitionName="fade"
-        transitionEnterTimeout={300}
-        transitionLeaveTimeout={300}
-        >
-          <Switch>
-            <Route exact path="/" component={AppWelcome}></Route>
-            <Route path="/dashboard" component={Dashboard}></Route>
-            <Route component={page404}/>
-          </Switch>
-        </CSSTransitionGroup>
+        <AppMain></AppMain>
       </div>
     );
   }
