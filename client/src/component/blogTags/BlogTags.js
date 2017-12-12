a1e77417c78b08374ada524a52f05a14523d8ebd
Haowen <zt452268020@live.com>
2017-12-12 21:48:50 +0800

    feat: update

--- client/src/component/blogTags/BlogTags.js
+++ client/src/component/blogTags/BlogTags.js
@@ -1,20 +1,53 @@
 import React, { Component } from 'react'
 import Axios from 'axios'
+import { Transition, TransitionGroup } from 'react-transition-group'
+
+import './BlogTags.css'
+
+const defaultStyle = {
+  transition: `all 300ms ease`,
+  opacity: 0
+}
+
+const transitionStyles = {
+  entering: { opacity: 0, transform: `translate3d(0, 30%, 0)` },
+  entered: { opacity: 1, transform: `translate3d(0, 0, 0)` }
+}
+
+const Tag = ({ children, index, tag }) => {
+  return (
+    <Transition in={true} appear={true} timeout={(100 + (100 * index))}>
+    {(state) => (
+      <li 
+      style={{
+        ...defaultStyle,
+        ...{transform: `translate3d(0, ${index * 10}%, 0)`},
+        ...transitionStyles[state]
+      }}>
+        {children}
+      </li>
+    )}
+  </Transition>
+  )
+}
 
 export default class BlogTags extends Component {
   constructor () {
     super()
     this.state = {
-      bookmarks: []
+      tags: []
     }
+  }
+  componentWillMount () {
+    this.getTags()
   }
   getTags () {
     Axios
       .get('/api/article/getTags')
       .then(({ data }) => {
-        this.setState({ bookmarks: [] })
+        this.setState({ tags: [] })
         setTimeout(() => {
-          this.setState({ bookmarks: data })
+          this.setState({ tags: data })
         }, 0)
       })
       .catch((err) => {
@@ -22,10 +55,17 @@ export default class BlogTags extends Component {
       })
   }
   render() {
+    const { tags } = this.state
     return (
-      <div>
-        this is marker
-      </div>
+      <ul className="tags-list">
+      <TransitionGroup>
+          {
+            tags.map((tag, i) => (
+              <Tag tag={tag} index={i}>{tag}</Tag>
+            ))
+          }
+        </TransitionGroup>
+      </ul>
     )
   }
 }
