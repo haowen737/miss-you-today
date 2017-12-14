ef52353048a306a97930799595682156e68acf7f
Haowen <zt452268020@live.com>
2017-12-14 21:14:06 +0800

    fix: 部署过程中出现的一些问题

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const Koa = require('koa');
 const app = new Koa();
 const router = require('koa-router')();
 
-// router.use('/api', api.routes(), api.allowedMethods());
-
-// app.use(response_formatter('^/api'));
 app.use(require('koa-static')(__dirname + '/client/build'));
 
 app.listen(3302);
