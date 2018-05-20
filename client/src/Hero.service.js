a7167ea567272c240099d572931c71b27d4bb0b5
Haowen <haowen737@gmail.com>
2018-05-20 21:33:55 +0800

    fix: new poem in greet content

--- client/src/Hero.service.js
+++ client/src/Hero.service.js
@@ -26,25 +26,30 @@ const poem2 = [
   'I wanna find an empty road '
 ]
 const poem3 = [
-  'I\'m breathing, I\'m feeling alone',
-  'I know that you\'re leaving',
-  'While we\'re still undone',
-  'It\'s gone right before our faces',
-  'Gone without a trace',
-  'We had something special',
-  'I don\'t need no space'
+  '喜欢一张白色的mixtape能随时陪伴她 在她的iPod里播放 当每次离开家',
+  'shit 照片里的她每个眼神都fucked up 之外 侧脸有点胖胖的 短裤包著她的butt',
+  '她说她有感受 有时还会感动',
+  '那些韵脚轻点到她的笑点打中她的痛',
+  '台南台北有她的fever 梦想不是dealer 出入不用visa 品质从不cheaper',
+  '温哥华 荷兰 她都smoke 巴赛隆纳 泰北 她都chill',
+  'she a stoner , 在烟雾中活著对你吐著说著',
+  '" 该死的 人生苦短为何要疑惑呢？"',
+  '广播催促她走 不再有约束了她懂',
+  '没忘了把这首带走 也许会是她的寄托',
+  '或是流泪的理由 慢慢想起那曾经',
+  '吸了最后一口 听著这副歌她往后一走'
 ]
 const poem4 = [
-  'We were lovers for the first time ',
-  'Running all the red lights ',
-  'The middle finger was our peace sign ',
-  'We were sipping on emotions ',
-  'Smoking and inhaling every moment ',
-  'It was reckless and we owned it ',
-  'We were high and we were sober ',
-  'We were under, we were over ',
-  'We were young and now I\'m older ',
-  'But I\'d do it all again '
+  '思维从脑海闪过拿起笔条件反射捕获下所有的经过因为不想浪费每时每刻',
+  '从不给人生留下遗憾',
+  '路还没走一半',
+  '梦想不移换不习惯 如灵感从不离散',
+  '也有过低落想起种种衣食无忧',
+  '想平凡了此余生为何梦里喋喋无休',
+  '我走的每一处每一路每一步都终将成为动力',
+  '身上的每道疤每道痂施加梦想重力',
+  '开始明白失败后他拍我肩膀的用意',
+  '--我的世界 by NaCho & Chey E'
 ]
 
 const theme = [{
