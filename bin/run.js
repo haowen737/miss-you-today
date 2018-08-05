fc6c78c68a5eadd211261d83bfd13b7a8afc1f3c
Haowen <haowen737@gmail.com>
2018-08-05 19:25:59 +0800

    Separate SSL Run Script

--- bin/run.js
+++ bin/run.js
@@ -1,31 +1,20 @@
 const path = require('path')
-const fs = require('fs')
 const config = require(path.resolve(__dirname, '../config'))
 const app = require('../built/app')
 const http = require('http')
-const https = require('https')
 const chalk = require('chalk')
-const debug = require('debug')('http')
 
 const printf = console.log
 const PORT = config.port
-
-const options = {
-  key: fs.readFileSync(path.resolve(__dirname, '../../../../ssl/server.key')),
-  cert: fs.readFileSync(path.resolve(__dirname, '../../../../ssl/server.crt'))
-}
 
 const onListening = () => {
   printf(chalk.green(`server is listening on port ${chalk.blue(PORT)}, started at ${chalk.blue(config.env)} mode`))
 }
 const server = http.createServer(app.callback())
-const sslServer = https.createServer(options, app.callback())
 
 server.listen(PORT)
-sslServer.listen(3003)
 
 server.on('listening', onListening)
-sslServer.on('listening', () => { console.log('ssl server start') })
 app.on('error', (err, ctx) => {
   log.error('server error', err, ctx)
 });
---
+++ bin/runSSL.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const fs = require('fs')
+const config = require(path.resolve(__dirname, '../config'))
+const app = require('../built/app')
+const https = require('https')
+const chalk = require('chalk')
+
+const printf = console.log
+const PORT = config.port
+
+const options = {
+  key: fs.readFileSync(path.resolve(__dirname, '../../../../ssl/server.key')),
+  cert: fs.readFileSync(path.resolve(__dirname, '../../../../ssl/server.crt'))
+}
+
+const onListening = () => {
+  printf(chalk.green(`ssl server is listening on port ${chalk.blue(PORT)}, started at ${chalk.blue(config.env)} mode`))
+}
+const sslServer = https.createServer(options, app.callback())
+
+sslServer.listen(3003)
+
+sslServer.on('listening', onListening)
+app.on('error', (err, ctx) => {
+  log.error('server error', err, ctx)
+});
