800bea93f78e4bb7e8722b608d65d208a6b6074d
haowen's vultr <zt452268020@live.com>
2018-08-09 03:30:04 +0000

    Production Swagger Address

--- client/src/utils/swagger.js
+++ client/src/utils/swagger.js
@@ -2,7 +2,7 @@ import Swagger from 'swagger-client'
 import * as https from "https"
 
 const httpsAgent = new https.Agent({ rejectUnauthorized: false })
-const url = `${window.location.protocol}//localhost:3007/swagger.json`
+const url = `${window.location.protocol}//www.withyoufriends.com/swagger.json`
 
 const apiWrappper = function (apis) {
   if (!apis) {
